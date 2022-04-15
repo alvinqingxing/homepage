b2bda048ad8500d7ce01b810a1b973f061283124
Alvin Lim <alvinch@hawaii.edu>
2022-04-15 20:11:37 +0800

    Menu

--- src/script.js
+++ src/script.js
@@ -3,23 +3,15 @@ const about = document.getElementById("about");
 const publications = document.getElementById("publications");
 const code = document.getElementById("code");
 const contact = document.getElementById("contact");
-const header = document.querySelector("header");
 const aboutToggle = document.getElementById("aboutMenuItem");
 const publicationsToggle = document.getElementById("publicationsMenuItem");
 const codeToggle = document.getElementById("codeMenuItem");
 const contactToggle = document.getElementById("contactMenuItem");
-const menuToggle = document.querySelector(".toggle");
 
-header.style.display = "flex";
 about.style.display = "block";
 publications.style.display = "none";
 code.style.display = "none";
 contact.style.display = "none";
-
-menuToggle.addEventListener("click", () => {
-  menuToggle.classList.toggle("active");
-  showcase.classList.toggle("active");
-});
 
 aboutToggle.addEventListener("click", () => {
   about.style.display = "block";
