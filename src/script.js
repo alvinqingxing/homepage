14a6308ddc12b7202b334304caf44a2f3eae39b5
Alvin Lim <alvinch@hawaii.edu>
2022-04-15 20:39:50 +0800

    Code

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ const about = document.getElementById("about");
 const publications = document.getElementById("publications");
 const code = document.getElementById("code");
 const contact = document.getElementById("contact");
+const savvygranny = document.getElementById("savvygranny");
+const mrcocktail = document.getElementById("mrcocktail");
 
 about.style.display = "block";
 publications.style.display = "none";
@@ -12,6 +14,8 @@ const aboutToggle = document.getElementById("aboutMenuItem");
 const publicationsToggle = document.getElementById("publicationsMenuItem");
 const codeToggle = document.getElementById("codeMenuItem");
 const contactToggle = document.getElementById("contactMenuItem");
+const savvygrannyToggle = document.getElementById("savvygrannyToggle");
+const mrcocktailToggle = document.getElementById("mrcocktailToggle");
 
 aboutToggle.addEventListener("click", () => {
   about.style.display = "block";
@@ -32,6 +36,23 @@ codeToggle.addEventListener("click", () => {
   publications.style.display = "none";
   code.style.display = "block";
   contact.style.display = "none";
+  savvygranny.style.display = "block";
+  mrcocktail.style.display = "none";
+  mrcocktailToggle.style.opacity = "0.5";
+});
+
+savvygrannyToggle.addEventListener("click", () => {
+  savvygranny.style.display = "block";
+  mrcocktail.style.display = "none";
+  mrcocktailToggle.style.opacity = "0.3";
+  savvygrannyToggle.style.opacity = "1";
+});
+
+mrcocktailToggle.addEventListener("click", () => {
+  savvygranny.style.display = "none";
+  mrcocktail.style.display = "block";
+  mrcocktailToggle.style.opacity = "1";
+  savvygrannyToggle.style.opacity = "0.3";
 });
 
 contactToggle.addEventListener("click", () => {
