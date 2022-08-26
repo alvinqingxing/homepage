db9dd790f8a713b807296e0ab8c156f333414d3a
Alvin Lim <alvinch@hawaii.edu>
2022-08-27 00:30:08 +0800

    Remove Mister Cocktail

--- src/script.js
+++ src/script.js
@@ -34,43 +34,14 @@ contactToggle.addEventListener("click", () => {
 });
 
 const codeToggle = document.getElementById("codeMenuItem");
-const savvygranny = document.getElementById("savvygranny");
-const mrcocktail = document.getElementById("mrcocktail");
-const savvygrannyToggle = document.getElementById("savvygrannyToggle");
-const mrcocktailToggle = document.getElementById("mrcocktailToggle");
 
 codeToggle.addEventListener("click", () => {
   about.style.display = "none";
   publications.style.display = "none";
   code.style.display = "block";
   contact.style.display = "none";
-  savvygranny.style.display = "block";
-  savvygrannyToggle.style.opacity = "1";
-  mrcocktail.style.display = "none";
-  mrcocktailToggle.style.opacity = "0.3";
 });
 
-savvygrannyToggle.addEventListener("click", () => {
-  savvygranny.style.display = "block";
-  mrcocktail.style.display = "none";
-  mrcocktailToggle.style.opacity = "0.3";
-  savvygrannyToggle.style.opacity = "1";
-});
-
-mrcocktailToggle.addEventListener("click", () => {
-  savvygranny.style.display = "none";
-  mrcocktail.style.display = "block";
-  mrcocktailToggle.style.opacity = "1";
-  savvygrannyToggle.style.opacity = "0.3";
-});
 
 const jsShowNojsHide = document.getElementById("jsShowNojsHide");
-const savvygrannyJsHideNojsShow = document.getElementById(
-  "savvygrannyJsHideNojsShow"
-);
-const mrcocktailJsHideNojsShow = document.getElementById(
-  "mrcocktailJsHideNojsShow"
-);
 jsShowNojsHide.style.display = "block";
-savvygrannyJsHideNojsShow.style.display = "none";
-mrcocktailJsHideNojsShow.style.display = "none";
