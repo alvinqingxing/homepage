e7d01e965b9f0d84774c9ebe250838226a873e5f
alvinqingxing <alvinch@hawaii.edu>
2022-09-07 14:07:00 +0800

    Remove Savvy Granny

--- src/script.js
+++ src/script.js
@@ -1,42 +1,29 @@
 const about = document.getElementById("about");
 const publications = document.getElementById("publications");
-const code = document.getElementById("code");
 const contact = document.getElementById("contact");
 
 about.style.display = "block";
 publications.style.display = "none";
-code.style.display = "none";
 contact.style.display = "none";
 
 const aboutToggle = document.getElementById("aboutMenuItem");
 const publicationsToggle = document.getElementById("publicationsMenuItem");
-const codeToggle = document.getElementById("codeMenuItem");
 const contactToggle = document.getElementById("contactMenuItem");
 
 aboutToggle.addEventListener("click", () => {
   about.style.display = "block";
   publications.style.display = "none";
-  code.style.display = "none";
   contact.style.display = "none";
 });
 
 publicationsToggle.addEventListener("click", () => {
   about.style.display = "none";
   publications.style.display = "block";
-  code.style.display = "none";
-  contact.style.display = "none";
-});
-
-codeToggle.addEventListener("click", () => {
-  about.style.display = "none";
-  publications.style.display = "none";
-  code.style.display = "block";
   contact.style.display = "none";
 });
 
 contactToggle.addEventListener("click", () => {
   about.style.display = "none";
   publications.style.display = "none";
-  code.style.display = "none";
   contact.style.display = "block";
 });
